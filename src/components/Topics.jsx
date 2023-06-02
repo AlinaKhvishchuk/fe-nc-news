0541d8d634790f611456105b76761168f1c74898
Alina Khvishchuk <alina.khvishchuk@gmail.com>
2023-06-02 15:22:07 +0100

    Add Topic component

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,5 +1,46 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
+import { fetchTopics } from "../../utils";
 import ArticleList from "./ArticleList";
 
-const Topics = () => {};
+const Topics = () => {
+  const [currTopics, setCurrTopics] = useState();
+  useEffect(() => {
+    fetchTopics().then((topics) => setCurrTopics(topics));
+  }, []);
+
+  if (!currTopics) return null;
+
+  if (currTopics) {
+    return (
+      <ul className="topics-list">
+        {currTopics.map(({ slug, description }) => {
+          return (
+            <li key={slug}>
+              <Link to></Link>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+};
 export default Topics;
+
+//   // if (!categories) return null;
+
+//   if (categories) {
+//     return (
+//       <ul id="category-list">
+//         {categories.map(({ category_name }) => {
+//           return (
+//             <li key={category_name}>
+//               <Link to={`/items?category=${category_name}`}>
+//                 <div className="category-name">{category_name}</div>
+//               </Link>
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     );
+//   }
+// }
