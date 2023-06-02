9a5af3ad39fea6dcf3900dcc122311699f2b972a
Alina Khvishchuk <alina.khvishchuk@gmail.com>
2023-06-02 08:55:57 +0100

    Update ArticleVotes

--- src/components/ArticleVotes.jsx
+++ src/components/ArticleVotes.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import { patchArticleVotes } from "../../utils";
-const ArticleVotes = (article) => {
-  const { article_id, title, topic, created_at, article_img_url } = article;
+const ArticleVotes = ({ article }) => {
+  const { article_id, votes } = article;
   const [votesChange, setVotesChange] = useState(0);
   const incVotes = () => {
     setVotes((prevVotes) => currVotes + 1);
-    patchArticleVotes(article_id);
+    patchArticleVotes(article_id).then((article) => {});
   };
+
+  return (
+    <div>
+      <button onClick={incVotes}>Vote Up</button>
+      <button onClick={decVotes}>Vote Down</button>
+      <span>{votes + votesChange}</span> {/* Display the vote count */}
+    </div>
+  );
 };
 
 export default ArticleVotes;
