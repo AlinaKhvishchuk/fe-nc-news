dfda0169f529880f9a73ebc407d585d56f67d200
Alina Khvishchuk <alina.khvishchuk@gmail.com>
2023-06-02 10:12:12 +0100

    Upd ArticleVotes, add css

--- src/components/ArticleVotes.jsx
+++ src/components/ArticleVotes.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 import { patchArticleVotes } from "../../utils";
+import "../css/ArticleVotes.css";
 const ArticleVotes = ({ article }) => {
   const { article_id, votes } = article;
   const [votesChange, setVotesChange] = useState(0);
   const incVotes = () => {
-    setVotes((prevVotes) => currVotes + 1);
-    patchArticleVotes(article_id).then((article) => {});
+    setVotesChange((currVotes) => currVotes + 1);
+    patchArticleVotes(article_id).catch((err) => {
+      setVotesChange((currVotes) => currVotes - 1);
+    });
+  };
+  const decVotes = () => {
+    setVotesChange((currVotes) => currVotes - 1);
+    patchArticleVotes(article_id).catch((err) => {
+      setVotesChange((currVotes) => currVotes + 1);
+    });
   };
 
   return (
-    <div>
-      <button onClick={incVotes}>Vote Up</button>
-      <button onClick={decVotes}>Vote Down</button>
-      <span>{votes + votesChange}</span> {/* Display the vote count */}
+    <div className="votes">
+      <div className="button-vote">
+        <button onClick={incVotes}>Like</button>
+        <button onClick={decVotes}>Dislike</button>
+      </div>
+      <span>Total votes: {votes + votesChange}</span>
     </div>
   );
 };
